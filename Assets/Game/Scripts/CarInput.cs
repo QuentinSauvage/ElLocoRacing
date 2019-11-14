40227dbe0ed7cbf54ae6c5a614eebdee8dcffcd1
Romon Emile <emile_romon@ens.univ-artois.fr>
2019-11-14 10:13:17 +0100

    :construction:

---
+++ Assets/Game/Scripts/CarInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarInput : MonoBehaviour
+{
+    CarController m_carController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_carController = GetComponent<CarController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        m_carController.Move(x, y);
+    }
+}
