2140b4e597bfd6679c5ff907d27ec4c6f99bfa94
Emile Romon <emile.romon@gmail.com>
2019-12-21 18:17:22 +0100

    KillZones added to the scene

---
+++ Assets/Game/Scripts/KillZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.attachedRigidbody.transform.SetPositionAndRotation(new Vector3(-150, 1, -50), Quaternion.Euler(0,90,0));
+            //TODO use last checkpoint position and rotation
+        }
+    }
+}
