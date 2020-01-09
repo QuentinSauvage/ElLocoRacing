ab88ecdbb16f59d0473c1b918fc4b254fd66c0f5
Emile Romon <emile.romon@gmail.com>
2020-01-09 19:16:51 +0100

    :loud_sound: :loud_sound: :loud_sound:

--- Assets/Game/Scripts/RaceStartCamController.cs
+++ Assets/Game/Scripts/RaceStartCamController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class RaceStartCamController : MonoBehaviour
 {
@@ -6,25 +7,48 @@ public class RaceStartCamController : MonoBehaviour
     [SerializeField] private GameObject m_1Text;
     [SerializeField] private GameObject m_2Text;
     [SerializeField] private GameObject m_3Text;
+    [SerializeField] private GameObject m_GoText;
     [SerializeField] private GameObject m_ui;
     [SerializeField] private GameObject m_pause;
+    [SerializeField] private AudioClip m_beeps;
+
+    private AudioSource m_audioSource;
+    private Camera m_camera;
 
     private void Awake()
     {
         m_1Text.SetActive(false);
         m_2Text.SetActive(false);
         m_2Text.SetActive(false);
+        m_GoText.SetActive(false);
+        m_audioSource = GetComponent<AudioSource>();
+        m_camera = GetComponent<Camera>();
     }
 
-    public void AnimationEnd()
+    public void AnimationStart()
+    {
+        m_audioSource.PlayOneShot(m_beeps);
+    }
+
+    public IEnumerator AnimationEnd()
     {
         GameController gameController = GameObject.Find("Controller").GetComponent<GameController>();
         gameController.Pause = false;
         gameController.Unpause = true;
+        m_GoText.SetActive(true);
         m_1Text.SetActive(false);
         m_ui.SetActive(true);
         m_pause.SetActive(false);
+        m_camera.enabled = false;
+        yield return StartCoroutine(LateEnd());
+    }
+
+    IEnumerator LateEnd()
+    {
+        yield return new WaitForSeconds(1);
+        m_GoText.SetActive(false);
         gameObject.SetActive(false);
+        
     }
 
     public void print1()
