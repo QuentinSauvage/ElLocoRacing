6ce5adb80906afb418ec39ca9a70bb58c5054833
QuentinSauvage <sauvage.quentin62@gmail.com>
2020-01-10 03:22:42 +0100

    fixed map cursor (no rotation and weird position)

---
+++ Assets/Game/Scripts/MapCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCursor : MonoBehaviour
+{
+	public GameObject m_car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!m_car.activeSelf)
+		{
+			gameObject.SetActive(false);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = new Vector3(m_car.transform.position.x, m_car.transform.position.y + 1, m_car.transform.position.z);
+    }
+}
